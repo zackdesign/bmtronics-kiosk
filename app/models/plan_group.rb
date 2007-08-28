4b75febafefffdd59d75a876572587b174aa08c5
softwud <softwud@0e6f2960-3e2d-0410-a5f0-dd94f435c85d>
2007-08-28 12:11:32 +0000

    2007-08-28 Upload of changes for the Plan Group functionality (prior to meeting with client on 29/8/2007). Still requires some work / bug fixes to be done.

--- app/models/plan_group.rb
+++ app/models/plan_group.rb
@@ -3,12 +3,17 @@ class PlanGroup < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :description
 
-  has_many :plans
+  has_many :plans, :foreign_key => "plan_group"
 
   def categories_chk=(cat_strs)
     @local_categories_strs = cat_strs
 #    breakpoint
     self.categories = @local_categories_strs.join(',')
 #    breakpoint
+  end
+
+  def plans_ids=(new_plan_ids)
+    @local_new_plan_ids = new_plan_ids
+    self.plans = Plan.find(@local_new_plan_ids)
   end
 end
---
+++ test/unit/plan_phone_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class PlanPhoneTest < Test::Unit::TestCase
+  fixtures :plan_phones
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
