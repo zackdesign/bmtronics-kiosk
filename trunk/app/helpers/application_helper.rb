ab5d882370e433e1a93cdcd9f6ea9e8056ac6230
enrico.teotti <enrico.teotti@0e6f2960-3e2d-0410-a5f0-dd94f435c85d>
2007-10-08 23:49:41 +0000

    - options for controllers moved - list of attributes in floating div

--- trunk/app/helpers/application_helper.rb
+++ trunk/app/helpers/application_helper.rb
@@ -1,9 +1,21 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
   def link_to_new
     action_name = "new"
     action_name = "newgroup" if ["listgroups", "newgroup", "showgroup", "creategroup", "editgroup", "updategroup", "destroygroup"].include?(controller.action_name)
        
     link_to("new", :action => action_name)
+  end
+  
+  ##
+  # needed to setup modal window
+  def control_modal_setup
+    js = <<END_OF_STATEMENT
+          document.getElementsByClassName('modal').each(function(link){
+              new Control.Modal(link, {opacity: 0.8, position: 'relative', offsetTop: 20, hover: true});
+          });
+END_OF_STATEMENT
+    javascript_tag(js)
   end
 end
--- trunk/app/helpers/phones_helper.rb
+++ trunk/app/helpers/phones_helper.rb
@@ -1,2 +1,31 @@
 module PhonesHelper
+  
+  def link_to_phone_features(phone)
+    if phone.features.count > 0
+      link_to(phone.features.count, "#phone#{phone.id}_features", :class => "modal") + list_related_features(phone)
+    else
+      content_tag(:span, phone.features.count)
+    end
+  end
+  
+  def link_to_phone_accessories(phone)
+    if phone.accessories.count > 0
+      link_to(phone.accessories.count, "#phone#{phone.id}_accessories", :class => "modal") + list_related_accessories(phone)
+    else
+      content_tag(:span, phone.accessories.count)
+    end
+  end
+  
+  private
+  def list_related_features(phone)
+    result = ""
+    phone.features.each{|f| result << content_tag(:li, f.name)}
+    content_tag(:div, content_tag(:ul, result), :id => "phone#{phone.id}_features")
+  end
+  
+  def list_related_accessories(phone)
+    result = ""
+    phone.accessories.each{|a| result << content_tag(:li, a.name)}
+    content_tag(:div, content_tag(:ul, result), :id => "phone#{phone.id}_accessories")
+  end  
 end
