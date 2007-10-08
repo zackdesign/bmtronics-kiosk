69289639dd9bd715ec39d29cddc027c1d149defb
enrico.teotti <enrico.teotti@0e6f2960-3e2d-0410-a5f0-dd94f435c85d>
2007-10-08 06:43:37 +0000

    - added table style - change h1 style - cleaned up list.rhtml for accessories, phones, features

--- app/helpers/accessories_helper.rb
+++ app/helpers/accessories_helper.rb
@@ -1,2 +1,28 @@
 module AccessoriesHelper
+  
+  def show_picture_if_available(accessory)
+    unless accessory.picture_name.blank?
+      image_tag(url_for({ :action => 'thumbnail', :id => accessory.id }), :alt => accessory.picture_name)
+    else
+      content_tag(:span, "None")
+    end
+  end
+  
+  def link_to_archive_action(accessory)
+    if params[:action] == 'listarch'
+      link_to('Unarchive', { :action => 'unarchive', :id => accessory }, :confirm => "Unarchive this accessory ?", :method => :post)
+    else
+      confirm_message = accessory.phones.empty? ? "Archive this accessory ?" : "WARNING: This accessory is still used by #{accessory.phones.count} phone(s).\n\nArchive this accessory ?"
+      link_to('Archive', { :action => 'archive', :id => accessory }, :confirm => confirm_message, :method => :post)
+    end
+  end
+  
+  def link_to_delete(accessory)
+    confirm_message = accessory.phones.empty? ? "Permanently delete this accessory ?" : "WARNING: Deleting this accessory will also delete it from #{accessory.phones.count} phone(s).\n\nPermanently delete this accessory ?"
+    if params[:action] == 'listarch'
+      link_to 'Delete', { :action => 'deletearch', :id => accessory }, :confirm => confirm_message, :method => :post
+    else
+      link_to'Delete', { :action => 'delete', :id => accessory }, :confirm => confirm_message, :method => :post
+    end
+  end
 end
--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -1,2 +1,20 @@
 module FeaturesHelper
+  
+  def link_to_archive_action(feature)
+    if params[:action] == 'listarch'
+      link_to('Unarchive', { :action => 'unarchive', :id => feature }, :confirm => "Unarchive this feature ?", :method => :post)
+    else
+      confirm_message = feature.phones.empty? ? "Archive this feature ?" : "WARNING: This feature is still used by #{feature.phones.count} phone(s).\n\nArchive this feature ?"
+      link_to('Archive', { :action => 'archive', :id => feature }, :confirm => confirm_message, :method => :post)
+    end
+  end
+  
+  def link_to_delete(feature)
+    confirm_message = feature.phones.empty? ? "Permanently delete this feature ?" : "WARNING: Deleting this feature will also delete it from #{feature.phones.count} phone(s).\n\nPermanently delete this feature ?"
+    if params[:action] == 'listarch'
+      link_to('Delete', { :action => 'deletearch', :id => feature }, :confirm => confirm_message, :method => :post)
+    else
+      link_to('Delete', { :action => 'delete', :id => feature }, :confirm => confirm_message, :method => :post)
+    end
+  end
 end
