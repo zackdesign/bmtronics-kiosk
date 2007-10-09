8c0b592fe3b0855583f55f33a1e3fb76da0cfc9b
zack99 <zack99@0e6f2960-3e2d-0410-a5f0-dd94f435c85d>
2007-10-09 10:21:03 +0000

    New Plan Charges code - can now add, delete, and edit Charge types
    Remaining - need to be able to modify the charge fields associated with them

--- trunk/app/controllers/charges_controller.rb
+++ trunk/app/controllers/charges_controller.rb
@@ -9,13 +9,42 @@ class ChargesController < ApplicationController
     @charge_pages, @charges = paginate :charge_types, :per_page => 20
   end
   
+  def edit 
+    @charge = ChargeType.find(params[:id])
+  end
+  
+  def update
+    @charge = ChargeType.find(params[:id])
+
+    if @charge.update_attributes(params[:charge])
+      flash[:notice] = 'Charge type was successfully updated.'
+      redirect_to :action => 'show', :id => @charge
+    else
+      render :action => 'edit'
+    end
+  end
+  
   def new 
     @charge = ChargeType.new
+  end
+  
+  def create
+    @charge = ChargeType.new(params[:charge])
+    if @charge.save
+      flash[:notice] = 'Charge type was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
   end
   
   def destroy
     ChargeType.find(params[:id]).destroy
     redirect_to :action => 'list'
+  end
+  
+  def show
+    @charge = ChargeType.find(params[:id])
   end
 
 end
