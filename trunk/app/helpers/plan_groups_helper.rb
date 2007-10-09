82f81e85437e122059fa39e375c8a9995046fb16
enrico.teotti <enrico.teotti@0e6f2960-3e2d-0410-a5f0-dd94f435c85d>
2007-10-09 22:20:04 +0000

    plans hover list added to plan groups

--- trunk/app/helpers/plan_groups_helper.rb
+++ trunk/app/helpers/plan_groups_helper.rb
@@ -1,2 +1,17 @@
 module PlanGroupsHelper
+  
+  def link_to_group_plans(plan_group)
+    if plan_group.plans.count > 0
+      link_to(plan_group.plans.count, "#group#{plan_group.id}_features", :class => "modal") + list_related_plans(plan_group)
+    else
+      content_tag(:span, plan_group.plans.count)
+    end
+  end
+  
+  private
+  def list_related_plans(plan_group)
+    result = ""
+    plan_group.plans.each{|p| result << content_tag(:li, p.name)}
+    content_tag(:div, content_tag(:ul, result), :id => "group#{plan_group.id}_features")
+  end
 end
